cdfd003a6c8df6f6985d2b89adb1653b3105120d
SANTHIYADEVI T <142086924+SanthiyaThangaraj@users.noreply.github.com>
2025-03-25 18:49:45 +0530

    Add files via upload

---
+++ weather-clothing-advisor.py
@@ -0,0 +1,8 @@
+temp=float(input("What is the temperature in celsius?"))
+if (temp<10):
+    print("It's cold! wear a warm coat and scarf.")
+elif(temp>10 and temp<=25):
+    print("It's mild! a light jacket will do.")
+else:
+    print("It's hot! T-shirt would be perfect.")
+    
