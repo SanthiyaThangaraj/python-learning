41324a0119423b9ee7eed3701d8fdbed6e4d2083
SANTHIYADEVI T <142086924+SanthiyaThangaraj@users.noreply.github.com>
2025-03-25 19:58:44 +0530

    Delete reverse-sort-modify.py

--- reverse-sort-modify.py
+++
@@ -1,8 +0,0 @@
-name=['Alice','Bob','Charlie','David']
-name.reverse()
-print(name)
-mylist=['sandy','bob','david']
-mylist.sort()
-print(mylist)
-list1=[mylist[0].upper(),mylist[1].upper(),mylist[2].upper()]
-print(list1)
