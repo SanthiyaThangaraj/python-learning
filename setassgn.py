65cc2f0ab49ddaa76861c7371d609db886dd2184
SANTHIYADEVI T <142086924+SanthiyaThangaraj@users.noreply.github.com>
2025-03-25 19:59:22 +0530

    Delete setassgn.py

--- setassgn.py
+++
@@ -1,7 +0,0 @@
-set1={1,2,3,4}
-set2={3,5,4}
-set3={1,3,5}
-print(set1.union(set2).union(set3))#or use /
-print(set1.intersection(set2).intersection(set3))#or use &
-print(set1.difference(set2).difference(set3))#or use -
- 
