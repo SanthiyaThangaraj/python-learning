28234ea9fa40c2318b075162185f213aab4d2e4e
SANTHIYADEVI T <142086924+SanthiyaThangaraj@users.noreply.github.com>
2025-03-25 20:03:27 +0530

    Add files via upload

---
+++ reverse-sort-modify.py
@@ -0,0 +1,11 @@
+"""Given names = ["Alice", "Bob", "Charlie", "David"], show the Python code to:
+   * Reverse the list in-place
+   * Create a new list with names sorted alphabetically
+   * Modify the original list to have all names in uppercasename=['Alice','Bob','Charlie','David']"""
+name.reverse()
+print(name)
+mylist=['sandy','bob','david']
+mylist.sort()
+print(mylist)
+list1=[mylist[0].upper(),mylist[1].upper(),mylist[2].upper()]
+print(list1)
