50eb9b69af796a27bc4b2535dc6c036105b17bf5
SANTHIYADEVI T <142086924+SanthiyaThangaraj@users.noreply.github.com>
2025-03-25 20:00:50 +0530

    Add files via upload

---
+++ nested-list.py
@@ -0,0 +1,8 @@
+"""For the nested list matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]], write code to:
+   * Access the element at the second row and third column
+   * Extract the second row as a list
+   * Create a new list containing the first element of each nested listmatrix=[[1,2,3],[4,5,6],[7,8,9]]"""
+print(matrix[1][2])
+print(matrix[1])
+mylist=[matrix[0][0],matrix[1][0],matrix[2][0]]
+print(mylist)
