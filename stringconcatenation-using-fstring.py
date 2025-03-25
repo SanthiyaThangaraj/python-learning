ace40592a6513c0e0aef53e4ed21d88a3d762da0
SANTHIYADEVI T <142086924+SanthiyaThangaraj@users.noreply.github.com>
2025-03-25 19:43:55 +0530

    Add files via upload

---
+++ stringconcatenation-using-fstring.py
@@ -0,0 +1,6 @@
+#Write Python code to create a string variable containing your name and another string variable containing your age as a string. Then concatenate them with appropriate spacing to form a complete sentence.
+#Sample Output: I'm [My name], [age] years old
+
+name=input("Enter your name:")
+age=input("Enter your age:")
+print(f"My name is {name} and I am {age} years old")
