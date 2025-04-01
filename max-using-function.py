becdfffa699540d067b9b5b9918383cf63bec0b1
SANTHIYADEVI T <142086924+SanthiyaThangaraj@users.noreply.github.com>
2025-04-01 07:48:06 +0530

    Add files via upload

---
+++ max-using-function.py
@@ -0,0 +1,12 @@
+def max_of_three(a,b,c):
+    if a>b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    else:
+        return c
+
+a=int(input("Enter a :"))
+b=int(input("Enter b :"))
+c=int(input("Enter c :"))
+print (max_of_three(a,b,c))
