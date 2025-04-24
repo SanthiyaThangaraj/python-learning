b9fc21fdc1131524585fbede28eed3dc9e73dd3e
SANTHIYADEVI T <142086924+SanthiyaThangaraj@users.noreply.github.com>
2025-04-24 08:40:36 +0530

    Add files via upload

---
+++ employee-details.py
@@ -0,0 +1,16 @@
+#Create a class Employee with attributes name, id, and salary. Add a method to increment salary by a given percentage.
+
+class employee:
+    def emp(self,name,id,salary):
+        self.name=name
+        self.id=id
+        self.salary=salary
+    def add(self):
+        percent=int(input("Enter percent for increment: "))
+        insalary=self.salary*percent
+        self.salary+=insalary
+        print(f"{self.name},\n Your salary is incremented by {percent}\n Current salary: {self.salary}")
+
+company=employee()
+company.emp("Santhiya",95,10000)
+company.add()
