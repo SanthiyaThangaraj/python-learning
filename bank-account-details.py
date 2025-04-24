0caef78b654730cc6b9ce89b325d6d5dda8dfafb
SANTHIYADEVI T <142086924+SanthiyaThangaraj@users.noreply.github.com>
2025-04-24 08:40:54 +0530

    Add files via upload

---
+++ bank-account-details.py
@@ -0,0 +1,25 @@
+#Create a class BankAccount with attributes account_holder, balance. Include methods to deposit, withdraw, and check balance.
+
+class bankAccount:
+    def bank(self, acc_hold, balance):
+        self.acc_hold=acc_hold
+        self.balance=balance
+
+    def action(self):
+        activity=input("Enter your activity (deposit/withdraw/check balance): ")
+        match(activity):
+            case "deposit":
+                amount=int(input("Enter deposit amount: "))
+                self.balance+=amount
+                print(f"{amount} is added to {self.acc_hold}'s account")
+            case "withdraw":
+                amount=int(input("Enter withdraw amount: "))
+                self.balance-=amount
+                print(f"{amount} is withdraw from {self.acc_hold}'s account")
+            case "check balance":
+                print(f"Balance {self.balance} is in {self.acc_hold}'s account")
+
+money=bankAccount()
+money.bank("Santhiya", 1000)
+money.action()
+            
